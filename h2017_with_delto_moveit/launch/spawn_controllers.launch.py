86b4eab2deb1ea132c53580305efcf66f6c034c6
CHOJAEMIN <81763871+JaeMin98@users.noreply.github.com>
2025-09-03 17:15:36 +0900

    .
    .

--- h2017_with_delto_moveit/launch/spawn_controllers.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_spawn_controllers_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("h2017_with_delto_gripper", package_name="h2017_with_delto_moveit").to_moveit_configs()
-    return generate_spawn_controllers_launch(moveit_config)
